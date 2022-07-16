2f5c2512b5253693112aedb7ae3b5be88a3c736e
Armine <arminesargsyan1996@gmail.com>
2022-07-16 15:29:24 +0400

    cart component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {CommonModule} from "@angular/common";
 import {MenuNavComponent} from "./components/menu-nav/menu-nav.component";
+import {CartComponent} from "./components/cart/cart.component";
 
 const routes: Routes = [
   {
     path: 'menu/:id',
-    component: MenuNavComponent
+    component: MenuNavComponent,
+  },
+  {
+    path: 'cart',
+    component: CartComponent,
   },
   {
     path: '**',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { MainService } from "./services/main.service";
 import { HttpClientModule } from "@angular/common/http";
+import { CartComponent } from './components/cart/cart.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { HttpClientModule } from "@angular/common/http";
     ProductListingComponent,
     ProductSingleComponent,
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    CartComponent,
   ],
   imports: [
     BrowserModule,
