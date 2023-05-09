3b60e2d334f831f67f973f48d34acec69b1731f8
Armine <arminesargsyan1996@gmail.com>
2023-05-09 20:50:34 +0400

    refactored Ingredient model.

--- src/dashboard/models/ingredient.ts
+++ src/dashboard/models/ingredient.ts
@@ -1,29 +1,8 @@
-import {ADDING_TYPES} from "./adding";
+import {Adding} from "./adding";
 
-export class Ingredient {
-    id: number;
-    name_hy: string;
-    name_en: string;
-    name_ru: string;
-    category_id: number;
-    category_name: string;
-    in_stock: boolean;
-    price: number;
-    type: ADDING_TYPES;
+export class Ingredient extends Adding {
 
     constructor(data: any) {
-        this.id = data.id;
-        this.name_en = data.name_en;
-        this.name_hy = data.nname_hy;
-        this.name_ru = data.name_ru;
-        this.category_id = data.category_id;
-        this.category_name = data.category_name;
-        this.in_stock = data.in_stock;
-        this.price = data.price;
-        this.type = data.type;
-    }
-
-    static transform(data: any): Ingredient {
-        return new Ingredient(data);
+        super(data);
     }
 }
